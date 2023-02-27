c88d0779680d27154beb1fc58a6e8bbc25a26a49
Galang Aji Susanto <galang.aji@mekari.com>
2023-02-27 07:33:33 +0700

    Initial Commit

---
+++ Sample Project Sharing Sesssion Swift UI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sample Project Sharing Sesssion Swift UI
+//
+//  Created by MEKARI on 27/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample Project Sharing Sesssion Swift UI/Sample_Project_Sharing_Sesssion_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_Project_Sharing_Sesssion_Swift_UIApp.swift
+//  Sample Project Sharing Sesssion Swift UI
+//
+//  Created by MEKARI on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Sample_Project_Sharing_Sesssion_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
